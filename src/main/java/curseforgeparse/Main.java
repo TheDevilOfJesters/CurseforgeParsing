8953473f41dbb79140321a6ee05f3dd728a7b254
TheDevilOfJesters <39665009+TheDevilOfJesters@users.noreply.github.com>
2020-08-29 12:20:38 -0400

    fixed main

--- src/main/java/curseforgeparse/Main.java
+++ src/main/java/curseforgeparse/Main.java
@@ -9,15 +9,8 @@ import static curseforgeparse.utilities.SeleniumSetup.setupDriver;
 import static curseforgeparse.utilities.SeleniumSetup.teardown;
 
 public class Main {
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     public static Logger logger = Logger.getLogger(String.valueOf(Main.class));
-=======
-    static Logger logger = Logger.getLogger(String.valueOf(Main.class));
->>>>>>> 6054e2eaae031701dd91618839e677b33f53c5d1
-=======
-    static Logger logger = Logger.getLogger(String.valueOf(Main.class));
->>>>>>> 6e8418b506b5489d4c8e90736107f714ae19dd75
 
 
     public static void main(String[] args) {
